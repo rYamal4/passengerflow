0e6a6df7559f8f817045f4a1fce0f70326475d57
rashit <rashit@RIVERWOOD>
2025-09-03 00:53:49 +0300

    added controllers

---
+++ src/main/java/io/github/ryamal4/passengerflow/controller/StopsController.java
@@ -0,0 +1,10 @@
+package io.github.ryamal4.passengerflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stops")
+public class StopsController {
+    // TODO fill controller
+}
