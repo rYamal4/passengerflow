3a6e21e0145d179dd044002f3c8d52fb3672e80a
rashit <rashit@RIVERWOOD>
2025-09-04 00:02:41 +0300

    added main PassengerFlowApp class

---
+++ src/main/java/io/github/ryamal4/passengerflow/PassengerFlowApp.java
@@ -0,0 +1,11 @@
+package io.github.ryamal4.passengerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerFlowApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerFlowApp.class, args);
+    }
+}
