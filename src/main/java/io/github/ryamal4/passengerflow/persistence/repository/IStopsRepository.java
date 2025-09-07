33f69ac59dfa6faaaa629781980a684b129c63c1
rashit <rashit@RIVERWOOD>
2025-09-07 18:21:10 +0300

    code clean up

--- src/main/java/io/github/ryamal4/passengerflow/persistence/repository/IStopsRepository.java
+++ src/main/java/io/github/ryamal4/passengerflow/persistence/repository/IStopsRepository.java
@@ -2,19 +2,26 @@ package io.github.ryamal4.passengerflow.persistence.repository;
 
 import io.github.ryamal4.passengerflow.persistence.entities.StopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface IStopsRepository extends JpaRepository<StopEntity, Long> {
-    @Query(value = "SELECT s.*, " +
-            "(6371 * acos(cos(radians(:lat)) * cos(radians(s.lat)) * cos(radians(s.lon)" +
-            " - radians(:lon)) + sin(radians(:lat)) * sin(radians(s.lat)))) AS distance " +
-            "FROM stops s " +
-            "ORDER BY distance " +
-            "LIMIT :count",
-            nativeQuery = true)
+
+    String NEARBY_STOPS_QUERY = """
+            SELECT s.*,
+                   (6371 * acos(
+                       cos(radians(:lat)) * cos(radians(s.lat)) *
+                       cos(radians(s.lon) - radians(:lon)) +
+                       sin(radians(:lat)) * sin(radians(s.lat))
+                   )) AS distance
+            FROM stops s
+            ORDER BY distance
+            LIMIT :count
+            """;
+
+    @NativeQuery(value = NEARBY_STOPS_QUERY)
     List<StopEntity> getNearbyStops(Double lat, Double lon, int count);
 }
