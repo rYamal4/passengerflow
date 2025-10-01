9db0cc85c42337461b4f14a2805d2455d272394c
rashit <rashit@RIVERWOOD>
2025-10-02 00:52:12 +0300

    added annotation

--- src/main/java/io/github/ryamal4/passengerflow/PassengerFlowApp.java
+++ src/main/java/io/github/ryamal4/passengerflow/PassengerFlowApp.java
@@ -2,10 +2,14 @@ package io.github.ryamal4.passengerflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class PassengerFlowApp {
     public static void main(String[] args) {
         SpringApplication.run(PassengerFlowApp.class, args);
