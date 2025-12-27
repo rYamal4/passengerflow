54998dcd130622d4393a07be1b9b1517d0df3b07
rashit <rashit@RIVERWOOD>
2025-12-27 22:38:39 +0300

    fixed n + 1

--- src/main/java/io/github/ryamal4/passengerflow/repository/IBusRepository.java
+++ src/main/java/io/github/ryamal4/passengerflow/repository/IBusRepository.java
@@ -1,9 +1,16 @@
 package io.github.ryamal4.passengerflow.repository;
 
 import io.github.ryamal4.passengerflow.model.Bus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IBusRepository extends JpaRepository<Bus, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"busModel", "route"})
+    List<Bus> findAll();
 }
